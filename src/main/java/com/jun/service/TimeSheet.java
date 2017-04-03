cad64e43afb9908b6002ef038a4a8413ba5a0022
know <wangjun@whknow.cn>
2017-04-04 04:50:28 +0800

    考勤记录异常生成

---
+++ src/main/java/com/jun/service/TimeSheet.java
@@ -0,0 +1,149 @@
+package com.jun.service;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFDataFormatter;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class TimeSheet {
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@127.0.0.1:1521/myorcl";
+	private String user = "jun";
+	private String password = "junking";
+	private static Connection conn;
+
+	public static void main(String[] args) throws Exception {
+		TimeSheet jk = new TimeSheet();
+		List<Map<String, String>> data = jk.parseExcel("D:/3月考勤报表.xlsx");
+		System.out.println("共有" + data.size() + "条数据");
+		conn = jk.getConnection();
+		conn.setAutoCommit(false);
+		System.out.println("Conn:" + conn);
+		// 清表
+		Statement statemenet = conn.createStatement();
+		statemenet.execute("delete from time_sheet");
+		statemenet.close();
+		jk.superInsert(data);
+		conn.commit();
+		jk.close(conn);
+	}
+
+	public Connection getConnection() throws SQLException, ClassNotFoundException {
+		if (conn == null) {
+			Class.forName(driver);
+			Connection test = DriverManager.getConnection(url, user, password);
+			return test;
+		} else {
+			return conn;
+		}
+	}
+
+	public void close(Connection conn) throws SQLException {
+		if (conn != null) {
+			conn.close();
+		}
+	}
+
+	public List<Map<String, String>> parseExcel(String filePath) throws Exception {
+		File file = new File(filePath);
+		// 创建Excel对象，读取文件
+		Workbook workbook = null;
+		if (filePath.endsWith("xls")) {
+			workbook = new HSSFWorkbook(FileUtils.openInputStream(file));
+		} else if (filePath.endsWith("xlsx")) {
+			workbook = new XSSFWorkbook(FileUtils.openInputStream(file));
+		} else {
+			throw new Exception("文档类型错误！");
+		}
+		// 通过名字“Sheet0”获取工作表
+		// HSSFSheet sheet = workbook.getSheet("Sheet0");
+		// 读取默认第一个工作表sheet
+		Sheet sheet = workbook.getSheetAt(0);
+		// 最后一行行号
+		int lastRowNum = sheet.getLastRowNum();
+		// 将excle转换成List
+		List<Map<String, String>> excelData = new ArrayList<Map<String, String>>();
+		// 读取每一行
+		for (int i = 1; i < lastRowNum + 1; i++) {
+			Row row = sheet.getRow(i);
+			// 每一列存进Map
+			Map<String, String> rowData = new HashMap<String, String>();
+			for (int j = 0; j < 6; j++) {
+				rowData.put(getColumnCharName(j), parseRow(row, j));
+			}
+			excelData.add(rowData);
+		}
+		workbook.close();
+		return excelData;
+	}
+
+	public String getColumnCharName(int index) {
+		return String.valueOf((char) (65 + index));
+	}
+
+	public String parseRow(Row row, int j) throws Exception {
+		Cell cell = row.getCell(j);
+		if (cell != null) {
+			switch (cell.getCellType()) {
+			case Cell.CELL_TYPE_STRING:
+				return cell.getStringCellValue();
+			case Cell.CELL_TYPE_NUMERIC:
+				HSSFDataFormatter dataFormatter = new HSSFDataFormatter();
+				return dataFormatter.formatCellValue(cell);
+			case Cell.CELL_TYPE_FORMULA:
+				return cell.getCellFormula();
+			default:
+				return cell.getStringCellValue();
+			}
+		}
+		return "";
+	}
+
+	public void superInsert(List<Map<String, String>> data) throws SQLException {
+		String code, name, operweek, operdate, opertime;
+		String sql = "insert into time_sheet (id,code,name,operweek,operdate,opertime) values (seq_time_sheet.nextval,?,?,?,?,?)";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		int count = 0;
+		int all = data.size();
+		for (Map<String, String> map : data) {
+			code = map.get("A");
+			name = map.get("C");
+			operweek = map.get("D");
+			operdate = map.get("E");
+			opertime = map.get("F");
+			
+			if(code == null || "".equals(code)){
+				continue;
+			}
+
+			ps.setString(1, code);
+			ps.setString(2, name);
+			ps.setString(3, operweek);
+			ps.setString(4, operdate);
+			ps.setString(5, opertime);
+			ps.addBatch();
+
+			if (++count % 1000 == 0) {
+				ps.executeBatch();
+			}
+			System.out.println("进度:" + count + "/" + all);
+		}
+		ps.executeBatch();
+		ps.close();
+	}
+}
---
+++ src/main/java/com/jun/service/TimeSheetLate.java
@@ -0,0 +1,160 @@
+package com.jun.service;
+
+import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class TimeSheetLate {
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@127.0.0.1:1521/myorcl";
+	private String user = "jun";
+	private String password = "junking";
+	private static Connection conn;
+	private static String allNameSql = "select name from time_sheet group by name order by name";
+	private static String allDateSql = "select operdate from time_sheet group by operdate order by operdate";
+	private static SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+	public static List<Map<String, String>> errorMsg = new ArrayList<Map<String, String>>();
+
+	// 上班时间(晚于等于这个时间就算迟到)
+	public static String startTime = "08:34";
+	// 下班时间(早于等于这个时间就算早退)
+	public static String endTime = "17:28";
+	// 导出Excel路径
+	public static String path = "C:/Users/lx/Desktop/";
+
+	public static void main(String[] args) throws Exception {
+		TimeSheetLate late = new TimeSheetLate();
+		conn = late.getConnection();
+		List<String> allName = late.querySingle(allNameSql);
+		List<String> allDate = late.querySingle(allDateSql);
+		List<String> allTime = null;
+		for (String name : allName) {
+			for (String date : allDate) {
+				if (late.getAllTime(name, date).size() < 1) {
+					late.log(name, date, "未打卡", "");
+					continue;
+				}
+				allTime = late.getAllTime(name, date);
+				late.processorLate(allTime, name, date);
+			}
+		}
+		late.close(conn);
+		if(errorMsg.size() > 0){
+			late.outPutExcel(errorMsg, path);
+			System.out.println("检测完毕!请看异常信息表！");
+		}else{
+			System.out.println("检测完毕!没有异常！");
+		}
+	}
+
+	private void outPutExcel(List<Map<String, String>> errorMsg, String path) throws Exception {
+		// 创建HSSFWorkbook对象
+		HSSFWorkbook wb = new HSSFWorkbook();
+		// 创建HSSFSheet对象
+		HSSFSheet sheet = wb.createSheet("异常信息");
+		sheet.setColumnWidth(0, 5000);
+		sheet.setColumnWidth(1, 5000);
+		sheet.setColumnWidth(2, 5000);
+		sheet.setColumnWidth(3, 5000);
+		HSSFRow row = null;
+		// 创建HSSFRow对象
+		row = sheet.createRow(0);
+		row.createCell(0).setCellValue("姓名");
+		row.createCell(1).setCellValue("打卡日期");
+		row.createCell(2).setCellValue("异常类型");
+		row.createCell(3).setCellValue("情况说明");
+		Map<String,String> map = null;
+		for(int i = 0; i < errorMsg.size(); i++){
+			// 创建HSSFRow对象
+			row = sheet.createRow(i+1);
+			map = errorMsg.get(i);
+			// 创建HSSFCell对象
+			row.createCell(0).setCellValue(map.get("NAME"));
+			row.createCell(1).setCellValue(map.get("DATE"));
+			row.createCell(2).setCellValue(map.get("TYPE"));
+			row.createCell(3).setCellValue(map.get("REMARK"));
+		}
+		// 输出Excel文件
+		FileOutputStream output = new FileOutputStream(path+"考勤异常信息.xls");
+		wb.write(output);
+		output.flush();
+		wb.close();
+	}
+
+	private void log(String name, String date, String type, String remark) {
+		Map<String, String> msg = new HashMap<String, String>();
+		msg.put("NAME", name);
+		msg.put("DATE", date);
+		msg.put("TYPE", type);
+		msg.put("REMARK", remark);
+		errorMsg.add(msg);
+	}
+
+	private void processorLate(List<String> allTime, String name, String date) throws ParseException {
+		String first = allTime.get(0);
+		String last = allTime.get(allTime.size() - 1);
+		if (first.equals(last)) {
+			log(name, date, "只打卡一次", "打卡时间:" + first);
+			return;
+		}
+		if (formatter.parse(first).compareTo(formatter.parse(startTime)) >= 0) {
+			if(formatter.parse(first).compareTo(formatter.parse("12:00")) >= 0){
+				log(name, date, "上午未打卡", "首次打卡时间:" + first);
+			}else{
+				log(name, date, "迟到", "迟到时间:" + first);
+			}
+		}
+		if (formatter.parse(last).compareTo(formatter.parse(endTime)) <= 0) {
+			if(formatter.parse(last).compareTo(formatter.parse("12:00")) <= 0){
+				log(name, date, "下午未打卡", "最后打卡时间:" + last);
+			}else{
+				log(name, date, "早退", "早退时间:" + last);
+			}
+		}
+	}
+
+	private List<String> getAllTime(String name, String date) throws SQLException {
+		return querySingle("select opertime from time_sheet where name='" + name + "' and operdate='" + date + "' order by opertime");
+	}
+
+	public List<String> querySingle(String sql) throws SQLException {
+		List<String> list = new ArrayList<String>();
+		Statement state = conn.createStatement();
+		ResultSet result = state.executeQuery(sql);
+		while (result.next()) {
+			list.add(result.getString(1));
+		}
+		result.close();
+		state.close();
+		return list;
+	}
+
+	public Connection getConnection() throws SQLException, ClassNotFoundException {
+		if (conn == null) {
+			Class.forName(driver);
+			Connection test = DriverManager.getConnection(url, user, password);
+			return test;
+		} else {
+			return conn;
+		}
+	}
+
+	public void close(Connection conn) throws SQLException {
+		if (conn != null) {
+			conn.close();
+		}
+	}
+}
