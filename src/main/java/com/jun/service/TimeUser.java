7f720c7c860dae516d2d99632e3c55c642afa361
know <wangjun@whknow.cn>
2017-06-02 16:55:15 +0800

    Signed-off-by: know <wangjun@whknow.cn>

--- src/main/java/com/jun/service/TimeUser.java
+++ src/main/java/com/jun/service/TimeUser.java
@@ -20,7 +20,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class TimeSheet {
+public class TimeUser {
 	private String driver = "oracle.jdbc.driver.OracleDriver";
 	private String url = "jdbc:oracle:thin:@127.0.0.1:1521/myorcl";
 	private String user = "jun";
@@ -28,40 +28,17 @@ public class TimeSheet {
 	private static Connection conn;
 
 	public static void main(String[] args) throws Exception {
-		TimeSheet jk = new TimeSheet();
-		List<Map<String, String>> data = jk.parseExcel("D:/5月考勤.xls");
+		TimeUser jk = new TimeUser();
+		List<Map<String, String>> data = jk.parseExcel("D:/名单.xlsx");
 		System.out.println("共有" + data.size() + "条数据");
 		conn = jk.getConnection();
 		conn.setAutoCommit(false);
 		System.out.println("Conn:" + conn);
-		// 清表
-		Statement statemenet = conn.createStatement();
-		statemenet.execute("delete from time_sheet");
-		statemenet.close();
 		jk.superInsert(data);
 		conn.commit();
 		jk.close(conn);
-		
-		TimeSheetLate late = new TimeSheetLate();
-		late.outPut();
 	}
-
-	public Connection getConnection() throws SQLException, ClassNotFoundException {
-		if (conn == null) {
-			Class.forName(driver);
-			Connection test = DriverManager.getConnection(url, user, password);
-			return test;
-		} else {
-			return conn;
-		}
-	}
-
-	public void close(Connection conn) throws SQLException {
-		if (conn != null) {
-			conn.close();
-		}
-	}
-
+	
 	public List<Map<String, String>> parseExcel(String filePath) throws Exception {
 		File file = new File(filePath);
 		// 创建Excel对象，读取文件
@@ -82,7 +59,7 @@ public class TimeSheet {
 		// 将excle转换成List
 		List<Map<String, String>> excelData = new ArrayList<Map<String, String>>();
 		// 读取每一行
-		for (int i = 1; i < lastRowNum + 1; i++) {
+		for (int i = 0; i < lastRowNum + 1; i++) {
 			Row row = sheet.getRow(i);
 			// 每一列存进Map
 			Map<String, String> rowData = new HashMap<String, String>();
@@ -94,11 +71,52 @@ public class TimeSheet {
 		workbook.close();
 		return excelData;
 	}
+	
+	public Connection getConnection() throws SQLException, ClassNotFoundException {
+		if (conn == null) {
+			Class.forName(driver);
+			Connection test = DriverManager.getConnection(url, user, password);
+			return test;
+		} else {
+			return conn;
+		}
+	}
 
+	public void close(Connection conn) throws SQLException {
+		if (conn != null) {
+			conn.close();
+		}
+	}
+	
+	public void superInsert(List<Map<String, String>> data) throws SQLException {
+		String id, name, dept;
+		String sql = "insert into time_user (id,name,dept) values (?,?,?)";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		int count = 0;
+		int all = data.size();
+		for (Map<String, String> map : data) {
+			dept = map.get("A");
+			name = map.get("B");
+			id = map.get("C");
+
+			ps.setString(1, id);
+			ps.setString(2, name);
+			ps.setString(3, dept);
+			ps.addBatch();
+
+			if (++count % 1000 == 0) {
+				ps.executeBatch();
+			}
+			System.out.println("进度:" + count + "/" + all);
+		}
+		ps.executeBatch();
+		ps.close();
+	}
+	
 	public String getColumnCharName(int index) {
 		return String.valueOf((char) (65 + index));
 	}
-
+	
 	public String parseRow(Row row, int j) throws Exception {
 		Cell cell = row.getCell(j);
 		if (cell != null) {
@@ -115,38 +133,5 @@ public class TimeSheet {
 			}
 		}
 		return "";
-	}
-
-	public void superInsert(List<Map<String, String>> data) throws SQLException {
-		String code, name, operweek, operdate, opertime;
-		String sql = "insert into time_sheet (id,code,name,operweek,operdate,opertime) values (seq_time_sheet.nextval,?,?,?,?,?)";
-		PreparedStatement ps = conn.prepareStatement(sql);
-		int count = 0;
-		int all = data.size();
-		for (Map<String, String> map : data) {
-			code = map.get("A");
-			name = map.get("C");
-			operweek = map.get("D");
-			operdate = map.get("E");
-			opertime = map.get("F");
-			
-			if(code == null || "".equals(code)){
-				continue;
-			}
-
-			ps.setString(1, code);
-			ps.setString(2, name);
-			ps.setString(3, operweek);
-			ps.setString(4, operdate);
-			ps.setString(5, opertime);
-			ps.addBatch();
-
-			if (++count % 1000 == 0) {
-				ps.executeBatch();
-			}
-			System.out.println("进度:" + count + "/" + all);
-		}
-		ps.executeBatch();
-		ps.close();
 	}
 }
