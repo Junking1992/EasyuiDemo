3c67ee29db301a3c63c6cc289763f5f8703e29ec
know <wangjun@whknow.cn>
2017-08-03 23:11:09 +0800

    2017年8月3日张哲修改

--- src/main/java/com/jun/service/OpeningByXlsTubGrade.java
+++ src/main/java/com/jun/service/OpeningByXlsTubGrade.java
@@ -2,7 +2,10 @@ package com.jun.service;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,13 +36,33 @@ public class OpeningByXlsTubGrade extends ProgressUtil {
 				//检查库号是否为纯数字
 				if(!storeCode.matches("[0-9]+")){
 					flag = false;
-					logMessage("Excel第" + errorRowNum + "行错误： 库号不能有汉字!");
+					logMessage("Excel第" + errorRowNum + "行错误： 库号不为纯数字!");
 				}
 				//检查桶号是否为纯数字
 				if(!tubCode.matches("[0-9]+")){
 					flag = false;
 					logMessage("Excel第" + errorRowNum + "行错误： 桶号不能有汉字!");
 				}
+				//1.取得库号对应PK，如果库号不存在抛出异常
+				String  pk_store="";
+				List list1=getPk_Store(storeCode);
+				if(list1.size()==1){
+					pk_store=(String) list1.get(0);
+				}else{
+					flag = false;
+					logMessage("Excel第" + errorRowNum + "行错误： 该库号没有唯一的对应主键!");
+				}
+				//2.根据三位桶号查询坛库存档完整桶号，如果不存在跳过本次循环
+				if(tubCode.length()>3){
+					flag = false;
+					logMessage("Excel第" + errorRowNum + "行错误： 该桶号大于三位数!");
+					
+				}else{
+					
+					
+				}
+				
+				//3.根据库号pk和完整桶号更新坛库存档和桶库存档的等级栏位
 			}
 			//如果flag为true就提交到数据库，否则回滚
 			if (flag) {
@@ -87,6 +110,29 @@ public class OpeningByXlsTubGrade extends ProgressUtil {
 			newGrade += "-";
 		}
 		return newGrade;
+	}
+	
+	//update
+	public int update(String sql) throws SQLException {
+		Statement Stmt = conn.createStatement();
+		// 返回新增或更新数据量
+		int i = Stmt.executeUpdate(sql);
+		Stmt.close();
+		return i;
+	}
+	
+	//select 
+	private List getPk_Store(String kuhao) throws SQLException {
+		String sql = "select pk_pubdoc from mtws_pubdoc  where code='"+kuhao+"' and dr=0;";
+		Statement Stmt = conn.createStatement();
+		ResultSet rs = Stmt.executeQuery(sql);
+		List<String> list =new ArrayList<String>();
+		while (rs.next()) {
+			list.add(rs.getString("pk_jar"));
+		}
+		Stmt.close();
+		rs.close();
+		return list;
 	}
 
 	private String driver = "oracle.jdbc.driver.OracleDriver";
